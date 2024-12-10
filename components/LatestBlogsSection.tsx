3d918ad5e86bdfe523f261d0d4ea57e7a2861d67
tf-shohag <shohag.techforing@gmail.com>
2024-12-10 11:08:52 +0600

    new blogs added

--- components/LatestBlogsSection.tsx
+++ components/LatestBlogsSection.tsx
@@ -11,14 +11,14 @@ export default function Homeblogsection() {
 
   return (
     <section className="dark:bg-gray-900 dark:text-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-6xl mx-auto">
+      <div className="container mx-auto">
         <div className="text-center mb-12">
           <h2 className="text-5xl font-bold mb-4">Latest from the Blog</h2>
           <p className="text-xl text-gray-400">Insights, tutorials, and tech discussions</p>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-8">
-        {allPostsData.slice(0,3).map((post, index) => (
+        {allPostsData.slice(0,6).map((post, index) => (
           <Card key={index} className="dark:bg-gray-800 dark:border-gray-700 overflow-hidden flex flex-col">
             <CardHeader className="p-0">
               <Image
